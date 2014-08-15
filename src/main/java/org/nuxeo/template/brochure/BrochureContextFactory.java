63edadd5dd87568558566983044973888be57048
Fred Vadon <fv@nuxeo.com>
2014-08-15 15:58:12 -0400

    brochure structure

---
+++ src/main/java/org/nuxeo/template/brochure/BrochureContextFactory.java
@@ -0,0 +1,18 @@
+package org.nuxeo.template.brochure;
+
+
+import java.util.Map;
+
+import org.nuxeo.ecm.core.api.DocumentModel;
+import org.nuxeo.template.api.context.ContextExtensionFactory;
+import org.nuxeo.template.api.context.DocumentWrapper;
+
+public class BrochureContextFactory implements ContextExtensionFactory {
+
+    @Override
+    public Object getExtension(DocumentModel currentDocument,
+            DocumentWrapper wrapper, Map<String, Object> ctx) {
+        return new BrochureContext(currentDocument, wrapper);
+    }
+
+}
